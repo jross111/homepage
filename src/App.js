6a0d3b8356c4869fee16f959bb65e85a4bc621e5
jross111 <jross.nyc111@gmail.com>
2019-08-19 04:53:40 -0400

    ugh

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBar from './NavBar' 
 import './App.css';
+import Blog from './Blog.js'
 import Button from 'react-bootstrap/Button';
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import Alert from 'react-bootstrap/Alert';
 import { FaBeer } from 'react-icons/fa';
+// import MDReactComponent from 'markdown-react-js';
 
 function App () {
   return (
@@ -16,6 +18,9 @@ function App () {
       <Jumbotron> <h1>James Ross<FaBeer /> </h1></Jumbotron>
         <Alert ></Alert>
         <Button variant='danger'>Hello</Button>
+
+
+
       </div>
 
 
---
+++ src/content/test.md
@@ -0,0 +1,11 @@
+# Test
+
+## Only a test
+
+**Bold**
+
+*Italic*
+
+[a link to yahoo](yahoo.com)
+
+[Test](#Test)
