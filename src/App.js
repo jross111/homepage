661dabfa016f9075f90577159cfe595172771660
jross111 <jross.nyc111@gmail.com>
2019-08-19 21:41:03 -0400

    refactored app.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 import NavBar from './NavBar' 
 import './App.css';
 // import Blog from './Blog.js'
-import Button from 'react-bootstrap/Button';
 import Jumbotron from 'react-bootstrap/Jumbotron'
-import Alert from 'react-bootstrap/Alert';
 import { FaBeer } from 'react-icons/fa';
 // import MDReactComponent from 'markdown-react-js';
 
@@ -14,12 +12,7 @@ class App extends Component {
     return (
       <div className="App">
       < NavBar />
-      <Jumbotron> <h1>James Ross<FaBeer /> </h1></Jumbotron>
-        <Alert ></Alert>
-        <Button variant='danger'>Hello</Button>
-
-
-
+      <Jumbotron fluid> <h1>James Ross<FaBeer /> </h1></Jumbotron>
       </div>
     )
   }
